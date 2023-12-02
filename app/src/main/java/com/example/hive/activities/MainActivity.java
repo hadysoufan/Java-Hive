e57b3cd0e24eeb5ef9e196cb839aa319bb320e4a
Hadi Soufan <hadisoufan74@gmail.com>
2023-12-02 13:38:39 +0200

    Getting Started

---
+++ app/src/main/java/com/example/hive/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.hive.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hive.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
